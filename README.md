7b56e76c75a9be51d2f297f8f46725742c18db6e
Joonas Tikkanen <joonas.tikkanen@ambientia.fi>
2024-01-11 19:59:02 +0200

    Separate different tasks as functions

--- README.md
+++ README.md
@@ -3,12 +3,68 @@
 ## Install dependencies
 
 ```bash
-sudo apt install -y python3-libcamera python3-kms++
-sudo apt install -y python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg python3-pip
-pip3 install -r requirements.txt
+sudo apt install -y python3-libcamera tesseract-ocr libtesseract-dev python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg python3-pip
 ```
 
 ```bash
 python3 -m venv venv
 source venv/bin/activate 
+pip3 install -r requirements.txt
+```
+
+## Install to Raspberry
+
+Copy files
+
+Create Systemd Service:
+
+```bash
+sudo nano /etc/systemd/system/watermeter.service
+```
+
+Create systemd file:
+
+```ini
+[Unit]
+Description=My Python Service
+
+[Service]
+ExecStart=/usr/bin/python3 /path/to/your/main.py
+
+Restart=always
+User=yourusername
+Environment=HOME_ASSISTANT_URL=http://10.20.30.20:8123
+Environment=HOME_ASSISTANT_TOKEN=your_token
+Environment=PICAMERA_IMAGE_PATH=/run/shm/watermeter_last.jpg
+Environment=PICAMERA_CONFIG='{"size": (1920, 1080)}'
+Environment=TESSERACT_PATH=/usr/bin/tesseract
+Environment=TESSERACT_CONFIG='--psm 6 -c tessedit_char_whitelist=0123456789'
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Replace `/path/to/your/main.py` with the actual path to your `main.py` file and `yourusername` with your actual username. Also, replace `your_token` with your actual Home Assistant token.
+
+3. Save the file and exit the editor.
+
+4. Reload the systemd manager configuration:
+
+```bash
+sudo systemctl daemon-reload
+```
+
+5. Enable your service to start on boot:
+
+```bash
+sudo systemctl enable watermeter
+```
+
+6. Start your service:
+
+```bash
+sudo systemctl start watermeter
+```
+
+Now, your Python script will run as a systemd service. It will start automatically when your system boots, and if it crashes, systemd will restart it.
 ```
--- main.py
+++ main.py
@@ -4,28 +4,89 @@ from picamera2 import Picamera2, Preview
 from libcamera import controls
 import cv2
 import pytesseract
+import requests
+import os
 
-camera = Picamera2()
-preview_config = camera.create_preview_configuration(main={"size": (1920, 1080)})
-camera.configure(preview_config)
-camera.start_preview(Preview.QTGL)
-camera.start()
-#camera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-sleep(300)
-camera.close()
+picamera_image_path = os.getenv('PICAMERA_IMAGE_PATH', '/run/shm/watermeter_last.jpg')
+picamera_config = os.getenv('PICAMERA_CONFIG', '{"size": (1920, 1080)}')
+terraseract_path = os.getenv('TESSERACT_PATH', '/usr/bin/tesseract')
+terrasact_config = os.getenv('TESSERACT_CONFIG', '--psm 6 -c tessedit_char_whitelist=0123456789')
+watermeter_last_value_file = os.getenv('WATERMETER_LAST_VALUE_FILE', '/run/shm/watermeter_last_value.txt')
+homeassistant_url = os.getenv('HOME_ASSISTANT_URL', 'http://10.20.30.20:8123')
+homeassistant_token = os.getenv('HOME_ASSISTANT_TOKEN')
 
-# Set the path to the tesseract executable
-# On Windows, this is typically 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+def main():
+    sensor_data = load_sensor_data()
+    take_picture()
+    read_image()
+    send_sensor_data_to_homeassistant(sensor_data)
 
-# Load the image from file
-image = cv2.imread('image.png')
+if __name__ == '__main__':
+    while True:
+        main()
+        sleep(300)  # Sleep for 300 seconds (5 minutes)
 
-# Convert the image to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def load_sensor_data():
+    try:
+        with open(watermeter_last_value_file, 'r') as f:
+            sensor_data = int(f.read())
+    except FileNotFoundError:
+        sensor_data = 0
 
-# Use Tesseract to do OCR on the image
-text = pytesseract.image_to_string(gray_image, config='--psm 6 -c tessedit_char_whitelist=0123456789')
+    return sensor_data
 
-# Print the text
-print(text)
+def take_picture():
+    camera = Picamera2()
+    preview_config = camera.create_preview_configuration(main=picamera_config)
+    camera.configure(preview_config)
+    # Turn on LED
+    #led.on()
+    # Turn on Camera and allow to adjust to brightness
+    camera.start_preview(Preview.NULL)
+    camera.start()
+    sleep(1)
+    # Take an image. I put in in /run/shm to not wear the SD card
+    metadata = camera.capture_file(picamera_image_path)
+    camera.close()
+    #led.off()
+    return True
+
+def read_image():
+    # Set the path to the tesseract executable
+    pytesseract.pytesseract.tesseract_cmd = terraseract_path
+
+    # Load the image from file
+    image = cv2.imread(picamera_image_path)
+
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Use Tesseract to do OCR on the image
+    sensor_data = pytesseract.image_to_string(gray_image, config=terrasact_config)
+    # Wire sensor data to file
+    with open('', 'w') as f:
+        f.write(str(sensor_data))
+
+    # Print the text
+    print(sensor_data)
+
+# Send the data to Home Assistant
+def send_sensor_data_to_homeassistant(sensor_data):
+    url = "http://{HOME_ASSISTANT_URL}/api/states/sensor.water_meter"
+    headers = {
+        "Authorization": "Bearer {homeassistant_token}",
+        "content-type": "application/json",
+    }
+    data = {
+        "state": sensor_data,
+        "attributes": {
+            "unit_of_measurement": "liters",
+            "friendly_name": "Water Meter",
+        }
+    }
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("Data sent to Home Assistant successfully. Sensor data: {sensor_data}")
+    else:
+        print(f"Failed to send data to Home Assistant. Status code: {response.status_code}")
